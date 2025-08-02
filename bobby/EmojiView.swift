8a20b2f0c44b23ea0a5cf9ff22ed0d711df0e39f
pagejenna <jwjx8888@gmail.com>
2025-08-02 10:40:08 +0800

    refactored stickman and emojiview into their own files

---
+++ bobby/EmojiView.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiView.swift
+//  bobby
+//
+//  Created by jenna on 2/8/25.
+//
+
+import SwiftUI
+
+struct EmojiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmojiView()
+}
---
+++ bobby/Stickman.swift
@@ -0,0 +1,18 @@
+//
+//  Stickman.swift
+//  bobby
+//
+//  Created by jenna on 2/8/25.
+//
+
+import SwiftUI
+
+struct Stickman: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Stickman()
+}
